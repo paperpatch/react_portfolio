ba7a17fe8ff78fd2ec864eb1d405e4be5a816477
paperpatch <patchen21@gmail.com>
2022-02-01 17:03:41 -0500

    rewrote other projects code

--- src/components/Other/index.js
+++ src/components/Other/index.js
@@ -1,6 +1,60 @@
 import React from "react"
 
 function Other() {
+
+  const projects = {
+    other: [
+      {
+        id: "tech_blog",
+        name: "Tech Blog",
+        github: "https://github.com/paperpatch/tech_blog",
+        deploy: "https://tech-blog-proj.herokuapp.com/",
+        description: "Built a CMS (Content Management System) style blog site. Uses Model-View-Controller paradigm and Sequelize.",
+        skills: "JS Node Express MySQL Handlebars Heroku"
+      },
+      {
+        id: "note_taker",
+        name: "Note Taker",
+        github: "https://github.com/paperpatch/note_taker",
+        deploy: "https://notetaker-express-proj.herokuapp.com/",
+        description: "Created an application to write and save notes. Uses Express.js back end to save and retrieve note data from JSON file.",
+        skills: "JavaScript Node Express Heroku"
+      },
+      {
+        id: "team_profile_generator",
+        name: "Team Profile Generator",
+        github: "https://github.com/paperpatch/team_profile_generator",
+        deploy: "https://github.com/paperpatch/team_profile_generator",
+        description: "Created a Node.js command line app that dynamically generates a team profile based on inputs.",
+        skills: "JS Node Jest Inquirer"
+      },
+      {
+        id: "weather_dashboard",
+        name: "Weather Dashboard",
+        github: "https://github.com/paperpatch/weather_dashboard",
+        deploy: "https://paperpatch.github.io/weather_dashboard/",
+        description: "Uses OpenWeather One Call and OpenWeather Current API to retrieve weather data for cities.",
+        skills: "JS Boostrap jQuery OpenWeatherAPI"
+      },
+      {
+        id: "work_day_scheduler",
+        name: "Work Day Scheduler",
+        github: "https://github.com/paperpatch/schedule_workday",
+        deploy: "https://paperpatch.github.io/schedule_workday/",
+        description: "A simple calendar application that allows a user to save events for each hour of the day.",
+        skills: "JS Bootstrap jQuery"
+      },
+      {
+        id: "employee_tracker",
+        name: "Employee Tracker",
+        github: "https://github.com/paperpatch/employee_tracker",
+        deploy: "https://github.com/paperpatch/employee_tracker",
+        description: "Created a Command Line Interface App to manage a company's employee database. Uses Content Management Systems (CMS) to view and interact with databases.",
+        skills: "JS Node MySQL"
+      },
+    ]
+  }
+
   return (
 
     <section id="projects" className="projects fade-in">
@@ -9,155 +63,34 @@ function Other() {
       <div className="container">
         <div className="row justify-content-evenly">
 
-          <div className="card col-4 project-card justify-content-between">
-            <span>
-              <div className="card-body col-12">
-                <div className="row">
-                  <i className="col-10 bi bi-folder fa-3x"></i>
-                  <a href="https://tech-blog-proj.herokuapp.com/" className="col-2" target="_blank" rel="noreferrer"><i className="bi bi-box-arrow-up-right fa-2x"></i></a>
-                </div>
-                <a href="https://tech-blog-proj.herokuapp.com/" target="_blank" rel="noreferrer">
-                  <h5 className="card-title">Tech Blog</h5>
-                </a>
-                <p className="card-text">Built a CMS (Content Management System) style blog site. Uses Model-View-Controller paradigm and Sequelize.</p>
+          {projects.other.map( other => { 
+            return (
+              <>
+              <div className="card col-4 project-card justify-content-between">
+                <span>
+                  <div className="card-body col-12">
+                    <div className="row">
+                      <i className="col-10 bi bi-folder fa-3x"></i>
+                      <a href={other.deploy} className="col-2" target="_blank" rel="noreferrer"><i className="bi bi-box-arrow-up-right fa-2x"></i></a>
+                    </div>
+                    <a href={other.deploy} target="_blank" rel="noreferrer">
+                      <h5 className="card-title">{other.name}</h5>
+                    </a>
+                    <p className="card-text">{other.description}</p>
+                  </div>
+                </span>
+                <span>
+                  <div className="row">
+                    <p className="col-10 text-sub">{other.skills}</p>
+                    <a href={other.github} className="card-link col-2" target="_blank" rel="noreferrer">
+                      <i className="bi bi-github fa-lg"></i>
+                    </a>
+                  </div>
+                </span>
               </div>
-            </span>
-            <span>
-              <div className="row">
-                <p className="col-10 text-sub">JS Node Express MySQL Handlebars Heroku</p>
-                <a href="https://github.com/paperpatch/tech_blog" className="card-link col-2" target="_blank" rel="noreferrer">
-                  <i className="bi bi-github fa-lg"></i>
-                </a>
-              </div>
-            </span>
-          </div>
-
-          <div className="card col-4 project-card justify-content-between">
-            <span>
-              <div className="card-body col-12">
-                <div className="row">
-                  <i className="col-10 bi bi-folder fa-3x"></i>
-                  <a href="https://notetaker-express-proj.herokuapp.com/"
-                    className="col-2" target="_blank" rel="noreferrer"><i className="bi bi-box-arrow-up-right fa-2x"></i></a>
-                </div>
-                <a href="https://notetaker-express-proj.herokuapp.com/" target="_blank" rel="noreferrer">
-                  <h5 className="card-title">Note Taker</h5>
-                </a>
-                <p className="card-text">Created an application to write and save notes. Uses Express.js back end to save and retrieve note data from JSON file.</p>
-              </div>
-            </span>
-            <span>
-              <div className="row">
-                <p className="col-10 text-sub">JavaScript Node Express Heroku</p>
-                <a href="https://github.com/paperpatch/note_taker" className="card-link col-2" target="_blank" rel="noreferrer">
-                  <i className="bi bi-github fa-lg"></i>
-                </a>
-              </div>
-            </span>
-          </div>
-
-          <div className="card col-4 project-card justify-content-between">
-            <span>
-              <div className="card-body col-12">
-                <div className="row">
-                  <i className="col-10 bi bi-folder fa-3x"></i>
-                  <a href="https://github.com/paperpatch/team_profile_generator" className="col-2" target="_blank" rel="noreferrer">
-                    <i className="bi bi-box-arrow-up-right fa-2x"></i>
-                  </a>
-                </div>
-                <a href="https://github.com/paperpatch/team_profile_generator" target="_blank" rel="noreferrer">
-                  <h5 className="card-title">Team Profile Generator</h5>
-                </a>
-                <p className="card-text">Created a Node.js command line app that dynamically generates a team profile based on inputs.</p>
-              </div>
-            </span>
-            <span>
-              <div className="row">
-                <p className="col-10 text-sub">JS Node Jest Inquirer</p>
-                <a href="https://github.com/paperpatch/team_profile_generator" className="card-link col-2" target="_blank" rel="noreferrer">
-                  <i className="bi bi-github fa-lg"></i>
-                </a>
-              </div>
-            </span>
-          </div>
-
-          <div className="card col-4 project-card justify-content-between">
-            <span>
-              <div className="card-body col-12">
-                <div className="row">
-                  <i className="col-10 bi bi-folder fa-3x"></i>
-                  <a href="https://paperpatch.github.io/weather_dashboard/" className="col-2" target="_blank" rel="noreferrer">
-                    <i className="bi bi-box-arrow-up-right fa-2x"></i>
-                  </a>
-                </div>
-                <a href="https://paperpatch.github.io/weather_dashboard/" target="_blank" rel="noreferrer">
-                  <h5 className="card-title">Weather Dashboard</h5>
-                </a>
-                <p className="card-text">Uses OpenWeather One Call and OpenWeather Current API to retrieve weather data for
-                  cities.</p>
-              </div>
-            </span>
-            <span>
-              <div className="row">
-                <p className="col-10 text-sub">JS Boostrap jQuery OpenWeatherAPI</p>
-                <a href="https://github.com/paperpatch/weather_dashboard" className="card-link col-2" target="_blank" rel="noreferrer">
-                  <i className="bi bi-github fa-lg"></i>
-                </a>
-              </div>
-            </span>
-          </div>
-
-          <div className="card col-4 project-card justify-content-between">
-            <span>
-              <div className="card-body col-12">
-                <div className="row">
-                  <i className="col-10 bi bi-folder fa-3x"></i>
-                  <a href="https://paperpatch.github.io/schedule_workday/" className="col-2" target="_blank" rel="noreferrer">
-                    <i className="bi bi-box-arrow-up-right fa-2x"></i>
-                  </a>
-                </div>
-                <a href="https://paperpatch.github.io/schedule_workday/" target="_blank" rel="noreferrer">
-                  <h5 className="card-title">Work Day Scheduler</h5>
-                </a>
-                <p className="card-text">A simple calendar application that allows a user to save events for each hour of
-                  the day.</p>
-              </div>
-            </span>
-            <span>
-              <div className="row">
-                <p className="col-10 text-sub">JS Bootstrap jQuery</p>
-                <a href="https://github.com/paperpatch/schedule_workday" className="card-link col-2" target="_blank" rel="noreferrer">
-                  <i className="bi bi-github fa-lg"></i>
-                </a>
-              </div>
-            </span>
-          </div>
-
-          <div className="card col-4 project-card justify-content-between">
-            <span>
-              <div className="card-body col-12">
-                <div className="row">
-                  <i className="col-10 bi bi-folder fa-3x"></i>
-                  <a href="https://github.com/paperpatch/employee_tracker" className="col-2" target="_blank" rel="noreferrer">
-                    <i className="bi bi-box-arrow-up-right fa-2x"></i>
-                  </a>
-                </div>
-                <a href="https://github.com/paperpatch/employee_tracker" target="_blank" rel="noreferrer">
-                  <h5 className="card-title">Employee Tracker</h5>
-                </a>
-                <p className="card-text">Created a Command Line Interface App to manage a company's employee database. Uses Content Management Systems (CMS) to view and interact with databases.</p>
-              </div>
-            </span>
-            <span>
-              <div className="row">
-                <p className="col-10 text-sub">JS Node MySQL</p>
-                <a href="https://github.com/paperpatch/employee_tracker" className="card-link col-2" target="_blank" rel="noreferrer">
-                  <i className="bi bi-github fa-lg"></i>
-                </a>
-              </div>
-            </span>
-          </div>
-
+              </>
+            )
+          })}
         </div>
       </div>
     </section>
