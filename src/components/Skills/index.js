b2abc9db029278a35d391c2e17b1ecc4cee7fa3f
paperpatch <patchen21@gmail.com>
2022-02-01 15:11:20 -0500

    renamed icon assets. renamed photolist to skills

---
+++ src/components/Skills/index.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+
+const PhotoList = ({ category }) => {
+  const [photos] = useState([
+    {
+      name: 'HTML',
+      category: 'frontend'
+    },
+    {
+      name: 'CSS',
+      category: 'frontend'
+    },
+    {
+      name: 'JavaScript',
+      category: 'frontend'
+    },
+    {
+      name: 'Bootstrap',
+      category: 'frontend'
+    },
+    {
+      name: 'Handlebars',
+      category: 'frontend'
+    },
+    {
+      name: 'React',
+      category: 'frontend'
+    },
+    {
+      name: 'NodeJS',
+      category: 'frontend'
+    },
+    {
+      name: 'Jest',
+      category: 'backend'
+    },
+    {
+      name: 'MySQL',
+      category: 'backend'
+    },
+    {
+      name: 'MongoDB',
+      category: 'backend'
+    },
+    {
+      name: 'Heroku',
+      category: 'backend'
+    },
+    {
+      name: 'Git Bash',
+      category: 'tools'
+    },
+    {
+      name: 'GitHub',
+      category: 'tools'
+    },
+    {
+      name: 'npm',
+      category: 'tools'
+    },
+  ]);
+
+  const currentPhotos = photos.filter(photo => photo.category === category);
+
+  return (
+    <div>
+      <div className="flex-row">
+        {currentPhotos.map((image, i) => (
+          <img
+            src={require(`../../assets/icons/${category}/${i}.jpg`)}
+            alt={image.name}
+            className="col code-icon"
+            key={image.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PhotoList;
