e084f8029d43c43cb3ffba0abe29d0df3fe63810
paperpatch <patchen21@gmail.com>
2022-02-01 15:19:04 -0500

    added about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,13 +4,54 @@ import profileImage from "../../assets/profile/profile-pic-2.webp";
 function About() {
 
   return (
-    <section className="my-5" >
-      <h1 id="about">Who am I?</h1>
-      <img src={profileImage} className="my-2" style={{ width: "100%" }} alt="profile" />
-      <div className="my-2">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
-      </p>
+    <section id="about" class="about fade-in">
+      <h2 class="title">
+        About Me
+      </h2>
+      <div class="inner container">
+        <div class="row">
+          <div class="about-description col-lg-6 col-md-12">
+            <p>
+              Hello! My name is Pat and I enjoy creating things on the internet. My interest in coding started when a
+              friend introduced me and thought I would be a good fit. Previously a civil engineer, I decided to take a
+              career change for coding to pursue making things that would excite me.
+            </p>
+            <p>
+              I am looking forward to learning about machine algorithm scripting and artificial intelligence. I want to experience and explore where I stand in this
+              rapid technological developing world.</p>
+            <p>
+              Apart from coding, I enjoy sports like volleyball, cycling and ultimate. As for hobbies, I occassionally
+              play chess and board games on my free time.
+            </p>
+          </div>
+          <img class="col-lg-6 col-md-12 m-auto profile-pic" src={profileImage} alt="Pat Chen" />
+        </div>
+      </div>
+      <h2 class="title">Skills</h2>
+      <div class="container">
+        <div class="row row-cols-auto">
+          <h3 class="col my-auto">Front End:</h3>
+          <img class="col code-icon" src="./assets/images/icons/html-1.svg" alt="html5" />
+          <img class="col code-icon" src="./assets/images/icons/css-3.svg" alt="css3" />
+          <img class="col code-icon" src="./assets/images/icons/javascript-1.svg" alt="javascript" />
+          <img class="col code-icon" src="./assets/images/icons/bootstrap-5-1.svg" alt="bootstrap5" />
+          <img class="col code-icon" src="./assets/images/icons/handlebars-1.svg" alt="handlebars-1" />
+          <img class="col code-icon" src="./assets/images/icons/react-2.svg" alt="react" />
+        </div>
+        <div class="row row-cols-auto">
+          <h3 class="col my-auto">Back End:</h3>
+          <img class="col code-icon" src="./assets/images/icons/nodejs-1.svg" alt="nodeJS" />
+          <img class="col code-icon" src="./assets/images/icons/jest-2.svg" alt="jest" />
+          <img class="col code-icon" src="./assets/images/icons/mysql-6.svg" alt="mySQL" />
+          <img class="col code-icon" src="./assets/images/icons/mongodb-icon-1.svg" alt="mongoDB" />
+          <img class="col code-icon" src="./assets/images/icons/heroku-4.svg" alt="heroku" />
+        </div>
+        <div class="row row-cols-auto">
+          <h3 class="col my-auto">Tools:</h3>
+          <img class="col code-icon" src="./assets/images/icons/git-bash.svg" alt="git-bash" />
+          <div class="col code-icon"><i class="fab fa-github fa-3x"></i></div>
+          <img class="col code-icon" src="./assets/images/icons/npm.svg" alt="npm" />
+        </div>
       </div>
     </section>
   )
