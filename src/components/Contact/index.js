fd43f9cadab371df982c4c5ca9ee5c70243bf12c
paperpatch <patchen21@gmail.com>
2022-02-02 18:13:14 -0500

    added text to contact section. centerized

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -39,28 +39,33 @@ function ContactForm() {
   }
 
   return (
-    <section>
-      <h1 data-testid="h1tag">Get in Touch</h1>
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" defaultValue={name} onBlur={handleChange} name="name"/>
-        </div>
-        <div>
-          <label htmlFor="email">Email address:</label>
-          <input type="email" defaultValue={email} name="email" onBlur={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" defaultValue={message} rows="5" onBlur={handleChange} />
-        </div>
-        {errorMessage && (
+    <section id="contact" className="fade-in">
+      <div className="contacts">
+        <h1 data-testid="h1tag" className="title">Get in Touch</h1>
+        <p>I am looking for new opportunities to grow. My inbox is always open.</p>
+        <p>Whether you have a question or just want to say hi, I'll try my best to get back to you!</p>
+      
+        <form id="contact-form" onSubmit={handleSubmit}>
           <div>
-            <p className="error-text">{errorMessage}</p>
+            <label htmlFor="name">Name:</label>
+            <input type="text" defaultValue={name} onBlur={handleChange} name="name"/>
           </div>
-        )}
-        <button data-testid="button" type="submit">Submit</button>
-      </form>
+          <div>
+            <label htmlFor="email">Email address:</label>
+            <input type="email" defaultValue={email} name="email" onBlur={handleChange} />
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea name="message" defaultValue={message} rows="5" onBlur={handleChange} />
+          </div>
+          {errorMessage && (
+            <div>
+              <p className="error-text">{errorMessage}</p>
+            </div>
+          )}
+          <button data-testid="button" type="submit">Submit</button>
+        </form>
+      </div>
     </section>
     );
 }
