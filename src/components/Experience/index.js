cb24af28da9d4f7982c4979bbcf9b726ac5fa392
paperpatch <patchen21@gmail.com>
2022-02-09 14:42:02 -0500

    implemented Vertical timeline element

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
@@ -9,11 +10,11 @@ function Experience() {
         id: "bootcamp",
         name: "UConn Bootcamp",
         role: "Developer",
-        dates: "September 2021 - Present",
+        dates: "September 2021 - March 2022",
         link: "https://bootcamp.uconn.edu/",
-        description1: "Worked with a variety of languages, platforms, frameworks, and content management systems such as JavaScript, Node.JS, mySQL, MongoDB and React.",
-        description2: "Collaborated with a small team of student designers to build a multi page recipe app, that provides a set of recipe instructions and nutritional values using multiple sources of APIs.",
-        description3: "Experienced with developing and maintaining code for in-house and clients.",
+        description1: "Experienced with a variety of languages, platforms, frameworks, and content management systems.",
+        description2: "Collaborated with small teams of student designers to build a multi page recipe app and a Issue Tracker content management system.",
+        description3: "Developed and maintained code for in-house and clients.",
       },
       {
         id: "garg",
@@ -21,9 +22,9 @@ function Experience() {
         role: "Engineer",
         dates: "April 2016 - September 2021",
         link: "https://www.gargengineering.com/",
-        description1: "Inspection and documentation of all construction activities, working with Amtrak and Metro North force account project activities. Coordinated and implemented all engineering, technical, or administrative determinations.",
-        description2: "Office Engineer for development of schedules, estimates, constructability reviews, and value engineering. Duties include field coordination, utility coordination, and setting up databases.",
-        description3: "Responsible for logging data, submittals, and inquiries; managing and organizing documents including contracts and specifications; drafting responses for inquiries and reviewing engineer responses for specification conformity.",
+        description1: "Inspection and documentation of all construction activities. Coordinated all technical and administrative determinations.",
+        description2: "Office Engineer for development of schedules, estimates, constructability reviews, and value engineering.",
+        description3: "Responsible for reviewing data, submittals, and inquiries. Manage design plans and specifications for conformity.",
       },
       {
         id: "uconn",
@@ -32,7 +33,8 @@ function Experience() {
         dates: "Winter 2015",
         link: "https://uconn.edu/",
         description1: "Designed sidewalk for predestrians using AutoCAD. Performed calculations on culvert design for improvements on Route 89.",
-        description2: "Project leader for scheduling goals, deadlines, regulated cost estimates on design project through weekly meetings and discussions."
+        description2: "Project leader for scheduling goals, deadlines, regulated cost estimates on design project through weekly meetings and discussions.",
+        description3: "Implemented Computer-aided Design (CAD) for project design and presentation."
       },
     ]
   }
@@ -41,31 +43,43 @@ function Experience() {
 
     <section id="experience" className="jobs fade-in">
       <h2 className="title">Experience</h2>
-
-      {work.company.map( data => {
-          return (
-            <React.Fragment key={data.id}>
-              <article>
-                <h3>{data.role} @ <a href={data.deploy} target="_blank" rel="noreferrer">{data.name}</a></h3>
-                <p className="range">{data.dates}</p> <br />
-
-                <ul>
-                  <li>
-                    {data.description1}
-                  </li>
-                  <li>
-                    {data.description2}
-                  </li>
-                  <li>
-                    {data.description3}
-                  </li>
-                </ul>
-              </article>
-            </React.Fragment>
-          );
-        })}
-
+      <VerticalTimeline>
+        {work.company.map( data => {
+            return (
+              <React.Fragment key={data.id}>
+                <VerticalTimelineElement
+                  className="vertical-timeline-element--work"
+                  contentStyle={{ background: '#002b41', color: '#fff' }}
+                  contentArrowStyle={{ borderRight: '7px solid  #0dcaf0' }}
+                  date={data.dates}
+                  iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                  icon={<i className="fab fa-angular experience-icon"></i>}
+                >
+                  <h3 className="vertical-timeline-element-title">{data.name}</h3>
+                  <h4 className="vertical-timeline-element-subtitle">{data.role}</h4>
+                  <ul>
+                    <li>
+                      {data.description1}
+                    </li>
+                    <li>
+                      {data.description2}
+                    </li>
+                    <li>
+                      {data.description3}
+                    </li>
+                  </ul>
+                </VerticalTimelineElement>
+              </React.Fragment>
+            );
+          })}
+        <VerticalTimelineElement
+          iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+          icon={<i className="fab fa-angular experience-icon"></i>}
+        />
+      </VerticalTimeline>
     </section>
+
+    
   );
 }
 export default Experience;
