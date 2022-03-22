c9f6712ae2adaf86c0fd1709fa418e14b5d4dd4c
paperpatch <patchen21@gmail.com>
2022-03-22 14:54:59 -0400

    added email link in contact section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -76,6 +76,7 @@ function ContactForm() {
         <h1 data-testid="h1tag" className="title">Get in Touch</h1>
         <p>I am looking for new opportunities to grow. My inbox is open.</p>
         <p>Whether you have a question or just want to say hi, I'll try my best to get back to you!</p>
+        <p>You can contact me using the form below or email me at <a href="mailto: patchen21@gmail.com"   target="_blank">patchen21@gmail.com</a></p>
       
         <form className="form" ref={form} onSubmit={sendEmail}>
           <div>
