c9700d50109a5cdd0b2a080ce706c5a48c8bc549
Pat Chen <patchen21@gmail.com>
2024-02-12 16:40:29 -0500

    update basic info

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -5,32 +5,28 @@ function Intro() {
   return (
     <div className="intro container d-flex flex-column">
       <h1 className="intro-title">Hi, my name is</h1>
-      <h1 className="intro-name">Patrick Chen.</h1>
+      <h1 className="intro-name">Pat Chen.</h1>
       <span className="intro-text-slider">
         <Typed
-          strings={[
-            "Developer",
-            "Problem Solver",
-            "Engineer"
-          ]}
+          strings={["Developer", "Problem Solver", "Engineer"]}
           typeSpeed={60}
           backSpeed={30}
           backDelay={1100}
           loop
         />
       </span>
-      <p className="text-sub">Recently graduated from <a href="https://bootcamp.uconn.edu/" target="_blank"
-          rel="noreferrer">UConn Bootcamp</a>.
-      </p>
+      <p className="text-sub">Experienced Software Engineer</p>
       <div className="mt-auto arrow">
         <div className="row text-center">
           <hr className="col my-auto"></hr>
-          <a className="col-2 my-auto" href="#about"><i className="fas fa-angle-down fa-3x"></i></a>
+          <a className="col-2 my-auto" href="#about">
+            <i className="fas fa-angle-down fa-3x"></i>
+          </a>
           <hr className="col my-auto"></hr>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Intro
+export default Intro;
